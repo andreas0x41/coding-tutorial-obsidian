071666d4f95be0643e6e68d4b921f1b5f14cfa72
Andreas Patuzzi <patuzzi.andreas@gmail.com>
2025-01-22 09:40:49 +0100

    Quartz sync: Jan 22, 2025, 9:40 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    // baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false, //! preview when exporting     https://quartz.jzhao.xyz/features/social-images
